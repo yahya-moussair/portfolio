20718e054d70c3b5bf2b4bc2e4b4bd4b36f03dd3
yahya-mws <yahyamoussair05@gmail.com>
2025-05-28 14:35:14 +0100

    complete about section version desktop and mobile

--- src/components/home/heroSection.jsx
+++ src/components/home/heroSection.jsx
@@ -5,18 +5,20 @@ import { Link } from "react-router-dom";
 const HeroSection = () => {
   return (
     <>
-      <div className=" lg:px-10 px-0 lg:py-20 py-2 flex lg:flex-row flex-col gap-x-15 lg:gap-y-0 gap-y-10 justify-center items-center z-10 w-[80%] mx-auto">
-        <div className="lg:w-1/3 w-full">
+      <div className=" lg:px-10 px-0 lg:py-20 py-7 flex lg:flex-row flex-col gap-x-15 lg:gap-y-0 gap-y-10 justify-center items-center z-10 w-[80%] mx-auto">
+        <div className="lg:w-1/3 rounded-2xl w-full overflow-hidden lg:hover:shadow-2xl/10 lg:hover:shadow-white/100">
           <img
             src={images.heroImg}
             alt=""
-            className="w-full rounded-2xl shadow-2xl"
+            className="w-full rounded-2xl lg:hover:scale-110 z-10 transition-all duration-300"
           />
         </div>
         <div className="lg:w-1/2 w-full flex flex-col gap-y-6 lg:text-start text-center">
           <div className="flex flex-col gap-4">
             <h1 className="lg:text-5xl text-3xl font-bold">Hi, I'm Yahya</h1>
-            <p className="lg:text-3xl text-xl font-semibold">Full Satck Web Devloper</p>
+            <p className="lg:text-3xl text-xl font-semibold">
+              Full Satck Web Devloper
+            </p>
           </div>
           <p className="lg:text-xl text-[1rem]">
             "I create modern, responsive websites that blend clean design with
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import HeroSection from '../components/home/heroSection';
+import About from '../components/home/about';
 
 const Home = () => {
     return (
         <>
             <div className="pt-30">
                 <HeroSection />
+                <About />
             </div>
         </>
     );
